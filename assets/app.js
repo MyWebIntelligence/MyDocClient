ba93c81bc831c7af3e98fbf368345a4103503f97
Jean Devalance <jean.devalance@gmail.com>
2021-11-06 14:21:02 +0100

    Init front-end (Webpack Encore)

---
+++ assets/app.js
@@ -0,0 +1,18 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file
+
+import './styles/app.scss'
+import './styles/fonts.scss'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+// Bootstrap
+import './custom_bootstrap'
+
+// start the Stimulus application
+//import './stimulus'
