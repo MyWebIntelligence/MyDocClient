983633e1bd1263c5a83f668ef0ff5c90edcd53a2
Jean Devalance <jean.devalance@gmail.com>
2021-12-03 12:23:43 +0100

    Base layout + index controller

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class IndexController extends AbstractController
 {
@@ -11,8 +14,13 @@ class IndexController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(Security $security): Response
     {
+        /** @var User $user */
+        if ($security->isGranted('IS_AUTHENTICATED_FULLY') && $user = $security->getUser()) {
+            return $this->redirectToRoute('user_projects');
+        }
+
         return $this->render('index/index.html.twig');
     }
 
