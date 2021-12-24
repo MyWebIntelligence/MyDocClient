e38fb00a2f048183e746617066561693cbd77cc6
Jean Devalance <jean.devalance@gmail.com>
2021-12-24 11:39:03 +0100

    User profile + layout

--- src/Controller/User/ProfileController.php
+++ src/Controller/User/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Controller\User;
 
 use App\Entity\User;
 use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,31 +12,45 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/user/profile")
+ * @IsGranted("IS_AUTHENTICATED")
+ */
 class ProfileController extends AbstractController
 {
     /**
-     * @Route("/user/profile", name="user_profile")
-     * @IsGranted("IS_AUTHENTICATED")
+     * @Route("", name="user_profile")
      */
-    public function index(Request $request, UserPasswordHasherInterface $passwordHasher): Response
+    public function index(
+        Request $request,
+        UserPasswordHasherInterface $passwordHasher,
+        EntityManagerInterface $entityManager): Response
     {
-        /**
-         * @var User $user
-         */
+        /** @var User $user */
         if ($user = $this->getUser()) {
             $form = $this->createForm(UserType::class, $user);
             $form->handleRequest($request);
 
             if ($form->isSubmitted()) {
-                $plainPassword = $form->get('changePassword')->get('plainPassword');
+                if ($form->isValid()) {
+                    $user->setIsVerified(false);
+                    $plainPassword = $form->get('changePassword')->get('plainPassword');
 
-                if (!empty($plainPassword->getData()) && $form->get('changePassword')->isValid()) {
-                    $user->setPassword(
-                        $passwordHasher->hashPassword(
-                            $user,
-                            $plainPassword->getData()
-                        )
-                    );
+                    if (!empty($plainPassword->getData()) && $form->get('changePassword')->isValid()) {
+                        $user->setPassword(
+                            $passwordHasher->hashPassword(
+                                $user,
+                                $plainPassword->getData()
+                            )
+                        );
+                        $this->addFlash('info', "Le mot de passe a été modifié.");
+                    }
+
+                    $entityManager->persist($user);
+                    $entityManager->flush();
+                    $this->addFlash('info', "Les informations ont été sauvegardées.");
+                } else {
+                    $entityManager->refresh($user);
                 }
             }
 
@@ -43,5 +58,9 @@ class ProfileController extends AbstractController
                 'form' => $form->createView(),
             ]);
         }
+
+        $this->addFlash('danger', "La page est inaccessible.");
+
+        return $this->redirectToRoute('home');
     }
 }
