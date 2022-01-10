85cf911a621196559a3b519af864b7a2ddf1cfa0
Jean Devalance <jean.devalance@gmail.com>
2022-01-10 12:54:20 +0100

    Updated document front -end component

--- assets/components/document/index.js
+++ assets/components/document/index.js
@@ -1,124 +1,121 @@
-import '../project/new-tag'
-import '../project/delete-tag'
-import '../project/rename-tag'
-import '../project/tag-tree'
-import {Modal} from 'bootstrap'
-
-import {marked} from 'marked'
+import '../project/new-tag';
+import '../project/delete-tag';
+import '../project/rename-tag';
+import '../project/tag-tree';
+import Modal from 'bootstrap/js/dist/modal';
+import {marked} from 'marked';
 
 window.addEventListener('load', () => {
-    const documentContent = document.getElementById('document_content')
+    const documentContent = document.getElementById('document_content');
     const initialContent = documentContent ? documentContent.value : '';
     const changesUnsaved = document.getElementById('changesUnsaved');
-    const initMetaBtn = document.getElementById('init_meta_btn')
-    const annotationSelection = document.getElementById('annotationSelection')
-    const preview = document.getElementById('markdownPreview')
-    const links = document.querySelectorAll('.og-preview')
-    const toolsModal = new Modal(document.getElementById('selectionToolsModal'))
+    const initMetaBtn = document.getElementById('init_meta_btn');
+    const annotationSelection = document.getElementById('annotationSelection');
+    const preview = document.getElementById('markdownPreview');
+    const toolsModal = Modal.getOrCreateInstance(document.getElementById('selectionToolsModal'));
+    const links = document.querySelectorAll('.og-preview');
 
     const extractOg = (response) => {
-        const parser = new DOMParser()
-        const html = parser.parseFromString(response, 'text/html')
-        const metas = html.querySelectorAll('meta')
+        const parser = new DOMParser();
+        const html = parser.parseFromString(response, 'text/html');
+        const metas = html.querySelectorAll('meta');
 
         metas.forEach(meta => {
-            const property = meta.getAttribute("property")
-            const content = meta.getAttribute("content")
+            const property = meta.getAttribute("property");
+            const content = meta.getAttribute("content");
 
             if (property !== null && property.startsWith('og:')) {
-                const type =  property.replace("og:", "")
-                console.log(type, content)
+                const type =  property.replace("og:", "");
             }
         })
     }
 
     const watchUnsaved = (event) => {
         if (event.target.value !== initialContent) {
-            changesUnsaved.classList.remove('visually-hidden')
+            changesUnsaved.classList.remove('visually-hidden');
         } else {
-            changesUnsaved.classList.add('visually-hidden')
+            changesUnsaved.classList.add('visually-hidden');
         }
     }
 
     const calculateOffset = (child, relativeOffset) => {
-        let parent = child.parentElement
-        const children = []
+        let parent = child.parentElement;
+        const children = [];
 
         if (parent.tagName !== 'P') {
-            parent = parent.closest('p')
-            child = child.parentElement
+            parent = parent.closest('p');
+            child = child.parentElement;
         }
 
-        for (let c of parent.childNodes) {
-            if (c === child) break
-            children.push(c)
+        if (parent) {
+            for (let c of parent.childNodes) {
+                if (c === child) break;
+                children.push(c);
+            }
         }
 
-        return relativeOffset + children.reduce((a, c) => a + c.textContent.length, 0)
+        return relativeOffset + children.reduce((a, c) => a + c.textContent.length, 0);
     }
 
-
     const selectText = (event) => {
-        let textSelection
+        let textSelection;
 
         if (event.target.value) {
-            textSelection = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd)
+            textSelection = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd);
         } else {
-            const text = event.target.innerText
-            const selection = window.getSelection()
-            const start = selection.anchorOffset
-            const end = selection.extentOffset
-            const anchorNode = selection.anchorNode
-            const extentNode = selection.extentNode
-            textSelection = text.substring(calculateOffset(anchorNode, start), calculateOffset(extentNode, end))
+            const text = event.currentTarget.innerText;
+            const selection = window.getSelection();
+            const start = selection.anchorOffset;
+            const end = selection.extentOffset;
+            const anchorNode = selection.anchorNode;
+            const extentNode = selection.extentNode;
+            textSelection = text.substring(calculateOffset(anchorNode, start), calculateOffset(extentNode, end));
         }
 
         if (textSelection !== '') {
-            annotationSelection.value = textSelection
-            toolsModal.show()
+            annotationSelection.value = textSelection;
+            toolsModal.show();
         } else {
-            annotationSelection.value = ''
+            annotationSelection.value = '';
         }
     }
 
     // Set Markdown preview
-    preview.innerHTML = marked.parse(preview.innerHTML)
+    preview.innerHTML = marked.parse(preview.innerHTML);
 
     if (documentContent) {
         const fetchMeta = () => {
             fetch(`/user/document/meta/${initMetaBtn.dataset.id}`)
                 .then(response => response.json())
                 .then(data => {
-                    documentContent.value = data.formatted
-                    changesUnsaved.classList.remove('invisible')
-                })
-        }
+                    documentContent.value = data.formatted;
+                    changesUnsaved.classList.remove('invisible');
+                });
+        };
 
-        documentContent.addEventListener('mouseup', selectText)
+        documentContent.addEventListener('mouseup', selectText);
 
         document.getElementById('confirmInitMeta').addEventListener('click', () => {
-            fetchMeta()
-        })
+            fetchMeta();
+        });
 
         // Init document meta-data
-        documentContent.addEventListener('change', watchUnsaved)
-        documentContent.addEventListener('keyup', watchUnsaved)
+        documentContent.addEventListener('change', watchUnsaved);
+        documentContent.addEventListener('keyup', watchUnsaved);
     }
 
-    if (preview) {
-        preview.addEventListener('mouseup', selectText)
-    }
+    // if (preview) {
+    //     preview.addEventListener('mouseup', selectText);
+    // }
 
     // Add Open Graph preview for links
-    /*
-    links.forEach((item) => {
-        item.addEventListener('mouseover', (event) => {
-            const url = event.target.getAttribute('href')
-            fetch(url, {
-                mode: 'no-cors',
-            }).then(res => res.text())
-                .then(data => extractOg(data))
-        })
-    })
-    */
-})
+    // links.forEach((item) => {
+    //     item.addEventListener('mouseover', (event) => {
+    //         const url = event.target.getAttribute('href');
+    //         fetch(url, {
+    //             mode: 'no-cors',
+    //         }).then(res => res.text())
+    //             .then(data => extractOg(data))
+    //     });
+    // });
+});
