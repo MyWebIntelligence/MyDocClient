6cbd89e4e1474e20ebfe47c72cdb2fa984851628
Jean Devalance <jean.devalance@gmail.com>
2022-02-18 11:50:54 +0100

    CSV export (meta + tags)

--- src/Controller/DownloadController.php
+++ src/Controller/DownloadController.php
@@ -2,19 +2,134 @@
 
 namespace App\Controller;
 
+use App\Entity\Document;
+use App\Entity\Project;
+use App\Repository\DocumentRepository;
+use JsonException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\HeaderUtils;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class DownloadController extends AbstractController
 {
+
+    private SluggerInterface $slugger;
+
+    public function __construct(SluggerInterface $slugger)
+    {
+        $this->slugger = $slugger;
+    }
+
     /**
-     * @Route("/download", name="download")
+     * @Route("/telechargements/{id}", name="download")
      */
-    public function index(): Response
+    public function index(Project $project): Response
     {
         return $this->render('download/index.html.twig', [
-            'controller_name' => 'DownloadController',
+            'project' => $project,
         ]);
+    }
+
+    /**
+     * @Route("/telecharger-document/{id}", name="download_document")
+     */
+    public function document(Document $document): Response
+    {
+        $response = new Response($document->getContent());
+        $disposition = HeaderUtils::makeDisposition(
+            HeaderUtils::DISPOSITION_ATTACHMENT,
+            sprintf('%s-%s.md',
+                $this->slugger->slug($document->getTitle()),
+                $document->getId()
+            )
+        );
+
+        $response->headers->set('Content-Disposition', $disposition);
+
+        return $response;
+    }
+
+    private function getDocuments(Project $project, Request $request, DocumentRepository $documentRepository)
+    {
+        $searchData = ['q' => ''];
+
+        if ($searchParams = $request->cookies->get('searchParams')) {
+            $searchData = json_decode($searchParams, true, 512, JSON_THROW_ON_ERROR);
+        }
+
+        $queryBuilder = $documentRepository->getSearchDocumentsQueryBuilder($project, $searchData['q']);
+
+        return $queryBuilder->getQuery()->getResult();
+    }
+
+    /**
+     * @Route("/telecharger-gexf/{id}", name="download_gexf")
+     */
+    public function gexf(Project $project, Request $request, DocumentRepository $documentRepository): Response
+    {
+        $documents = $this->getDocuments($project, $request, $documentRepository);
+
+        return new Response();
+    }
+
+    /**
+     * @Route("/telecharger-csv/{id}", name="download_csv")
+     * @throws JsonException
+     */
+    public function csv(Project $project, Request $request, DocumentRepository $documentRepository): Response
+    {
+        $documents = $this->getDocuments($project, $request, $documentRepository);
+        $headers = array_values(Document::getMetadataDict());
+        $headers[] = 'Tags';
+        $filename = sprintf("mydoc-export-csv-%s.csv", date("YmdHis"));
+        $filepath = sys_get_temp_dir() . DIRECTORY_SEPARATOR . $filename;
+        $file = fopen($filepath, 'wb+');
+
+        fputcsv($file, $headers, ";");
+
+        /** @var Document $document */
+        foreach ($documents as $document) {
+            $tags = [];
+
+            foreach ($document->getAnnotations() as $annotation) {
+                if ($tag = $annotation->getTag()) {
+                    $tags[] = $tag->getName();
+                }
+            }
+
+            $tags = array_unique($tags);
+
+            $data = [
+                $document->getTitle(),
+                $document->getCreator(),
+                $document->getContributor(),
+                $document->getCoverage(),
+                $document->getDate(),
+                $document->getDescription(),
+                $document->getSubject(),
+                $document->getType(),
+                $document->getFormat(),
+                $document->getIdentifier(),
+                $document->getLanguage(),
+                $document->getPublisher(),
+                $document->getRelation(),
+                $document->getRights(),
+                $document->getSource(),
+                implode(', ', $tags),
+            ];
+
+            fputcsv($file, $data, ";");
+        }
+
+        $response = new BinaryFileResponse($filepath);
+        $response->headers->set('Content-Type', 'text/csv');
+        $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, $filename);
+
+        return $response;
     }
 }
